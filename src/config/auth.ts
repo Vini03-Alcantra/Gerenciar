0bda9390f518072db4c9197535c036dd864f8598
vinicius de alcântra Barros <viniciusalcantarabarros@gmail.com>
2021-12-20 21:16:30 -0300

    Configurrando o auth

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret: "c5a915c152292b29797ce129719c5175",
+    expires_in_token: "30m",
+    secret_refresh_token: "e64b50f929ad1fd9f330f7783b3a0d25",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30
+}
