7afb3595ba089772b67027f928b8b762c9de6b0a
Vinícius de Alcantra <52285659+Vini03-Alcantra@users.noreply.github.com>
2022-07-24 11:45:52 -0300

    Calculate income about renda_variable

--- src/modules/balance/infra/repositories/BalanceRepository.ts
+++ src/modules/balance/infra/repositories/BalanceRepository.ts
@@ -11,7 +11,7 @@ class BalanceRepository implements IBalanceReposritory {
     private repositoryAccountConstant: Repository<AccountConstant>;                 
     private repositoryAccountsVariable: Repository<AccountVariable>;
     private repositoryRendaConstant: Repository<RendaConstant>;
-    private repositoryRendasVariableRepository: Repository<RendaVariavel>;
+    private repositoryRendasVariable: Repository<RendaVariavel>;
 
 
     private providerDate: DayjsDateProvider;
@@ -20,7 +20,7 @@ class BalanceRepository implements IBalanceReposritory {
         this.repositoryAccountConstant = getRepository(AccountConstant)
         this.repositoryAccountsVariable = getRepository(AccountVariable)
         this.repositoryRendaConstant = getRepository(RendaConstant)
-        this.repositoryRendasVariableRepository = getRepository(RendaVariavel)
+        this.repositoryRendasVariable = getRepository(RendaVariavel)
         this.providerDate = new DayjsDateProvider()
     }
     async balanceMomentMonth(person_id: string): Promise<Number> {
@@ -55,6 +55,15 @@ class BalanceRepository implements IBalanceReposritory {
 
             rendaFixaCurrentMonth.filter((item) => 
                 income += Number(item.valorRenda)
+            )
+
+            const rendaVariableCurrentMonth = await this.repositoryRendasVariable
+                .createQueryBuilder("renda_variavel")
+                .where("renda_variavel.id_person = :id_person AND renda_variavel.data_renda_variavel BETWEEN :first_day AND :today", {id_person: person_id, first_day: firstDayMonth, today: today})
+                .getMany()
+
+            rendaVariableCurrentMonth.filter((item) => 
+                income += Number(item.valorRendaVariavel)
             )
 
             console.log(income)
